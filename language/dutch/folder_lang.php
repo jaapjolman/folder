95471f557340fd823e480bedacd8fc7d4fb531d5
Jaap Jolman <jaap@jolman.eu>
2013-02-04 12:04:24 +0100

    Added Dutch Language

---
+++ language/dutch/folder_lang.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams.folder.name'] 						= 'Map';
+$lang['streams.folder.save_first']					= 'Eerst Opslaan A.U.B...';
+$lang['streams.folder.view_files']					= 'Bekijk Bestanden';
